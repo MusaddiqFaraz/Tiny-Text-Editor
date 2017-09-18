9ae932adaf5faebaa77854d6e6d8e03be31362e7
Syed Musaddiq Faraz <faraz@Creos-Mac-mini-3.local>
2017-09-18 11:53:24 +0530

    included filetypes

--- tinyTextEditor.c
+++ tinyTextEditor.c
@@ -254,7 +254,32 @@ int editorSyntaxToColor(int highlight) {
     }
 }
 
+void editorSelectSyntaxHighlight() {
+    E.syntax = NULL;
+    if(E.filename == NULL) return;
 
+    char *ext = strrchr(E.filename, '.');
+
+    for(unsigned int j = 0; j < HLDB_ENTRIES; j++) {
+        struct editorSyntax *s = &HLDB[j];
+        unsigned int i = 0;
+        while(s->filematch[i]) {
+            int is_ext = (s->filematch[i][0] == '.');
+            if((is_ext && ext && !strcmp(ext, s->filematch[i])) ||
+                (!is_ext && strstr(E.filename, s->filematch[i]))) {
+                    E.syntax = s;
+
+                    int filerow;
+                    for(filerow = 0; filerow < E.numrows; filerow++) {
+                        editorUpdateSyntax(&E.row[filerow]);
+                    }
+
+                    return;
+                }
+            i++;
+        }
+    }
+}
 
 /** row operations **/
 
@@ -437,6 +462,8 @@ void editorOpen(char *filename) {
     free(E.filename);
     E.filename = strdup(filename);
 
+    editorSelectSyntaxHighlight();
+
     FILE *fp = fopen(filename, "r");
     if(!fp) die("fopen");
 
@@ -465,6 +492,7 @@ void editorSave() {
             editorSetStatusMessage("Save aborted");
             return;
         }
+        editorSelectSyntaxHighlight();
     }
 
     int len;
